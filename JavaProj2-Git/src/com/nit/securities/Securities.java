d681499467701972f170c595610217640207bf82
prasadbodduarena <prasadnaiduboddu081@gmail.com>
2024-05-02 16:01:40 +0530

    securityIssues() added to Securitites branch and pushed!

--- JavaProj2-Git/src/com/nit/securities/Securities.java
+++ JavaProj2-Git/src/com/nit/securities/Securities.java
@@ -4,4 +4,7 @@ public class Securities {
 public String doSecurities() {
 	return "Securities issues got solved sir!";
 }
+public String securitiesIssues(boolean isSolved,double charges) {
+	return "issues are solved";
+}
 }
