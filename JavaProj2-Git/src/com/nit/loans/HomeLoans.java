d7ead582c017b8e11dcf10d1159fd55552f4594d
prasadbodduarena <prasadnaiduboddu081@gmail.com>
2024-05-02 15:19:06 +0530

    loan sanctioned ! yes boss!

---
+++ JavaProj2-Git/src/com/nit/loans/HomeLoans.java
@@ -0,0 +1,8 @@
+package com.nit.loans;
+
+public class HomeLoans {
+public String sanctionLoan(boolean isDocuments,double amount,String status) {
+	System.out.println("sanctionLoan is pending....");
+return "sanctionLoan is successfully done!";	
+}
+}
