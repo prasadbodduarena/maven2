4bc1041865d2c0925e2df94a1343db4d73cdabd2
prasadbodduarena <prasadnaiduboddu081@gmail.com>
2024-05-02 14:58:53 +0530

    transferMoney() is added brother!

--- JavaProj2-Git/src/com/nit/service/UPI_Banking.java
+++ JavaProj2-Git/src/com/nit/service/UPI_Banking.java
@@ -4,4 +4,7 @@ public class UPI_Banking {
 public String  payment(long contactNo,double amount) {
 	return "Payment is successful!";
 }
+public String transferMoney(long srcAcc,long destAcc,double amount) {
+	return "Money got transferred succussfully!";
+}
 }
