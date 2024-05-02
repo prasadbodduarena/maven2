e3de322d36aaebc628929c22d389affe9008b3c6
prasadbodduarena <prasadnaiduboddu081@gmail.com>
2024-05-02 15:01:15 +0530

    money is deposited successufully ! friends!

--- JavaProj2-Git/src/com/nit/service/UPI_Banking.java
+++ JavaProj2-Git/src/com/nit/service/UPI_Banking.java
@@ -4,4 +4,7 @@ public class UPI_Banking {
 public String  payment(long contactNo,double amount) {
 	return "Payment is successful!";
 }
+public String depositeMoney(long srcAcc,long destiAccc,double amount) {
+	return "money is successfully deposited! boss!";
+}
 }
